5236c8cb763c2bd856a14c7a12803c9a43742450
catalinjitea <catalinjitea@gmail.com>
2017-09-13 17:18:31 +0300

    Remove crontab temporary file

--- cronjobs-entrypoint.sh
+++ cronjobs-entrypoint.sh
@@ -7,4 +7,5 @@ echo "*/2 * * * * /var/local/pontaj/crontabs/run_snmp.sh && /var/local/pontaj/cr
 echo "0 6 * * 1-5 /var/local/pontaj/crontabs/run_clear_macs.sh" >> crontab.tmp
 
 crontab crontab.tmp
+rm crontab.tmp
 crond -f -d 0
