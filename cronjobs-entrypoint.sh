229893199b9c5bf8cce4d6fd18700b060da0493f
anton16 <anton@eaudeweb.ro>
2017-09-22 17:15:15 +0300

    Bugfix: crontab.tmp was appended at each container restart.

--- cronjobs-entrypoint.sh
+++ cronjobs-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Running cronjobs"
 
-echo "*/2 * * * * /var/local/pontaj/crontabs/run_snmp.sh && python /var/local/pontaj/manage.py check_insert_mac_addresses" >> crontab.tmp
+echo "*/2 * * * * /var/local/pontaj/crontabs/run_snmp.sh && python /var/local/pontaj/manage.py check_insert_mac_addresses" > crontab.tmp
 echo "0 6 * * 1-5 python /var/local/pontaj/manage.py clear_mac_addresses" >> crontab.tmp
 
 crontab crontab.tmp
