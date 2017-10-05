6a19da318f994ad39f0be0369f507c46e308c6df
catalinjitea <catalinjitea@gmail.com>
2017-10-05 18:02:54 +0300

    Add about page; add link to github issues

--- clocking/api/__init__.py
+++ clocking/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask.ext.script import Manager
 
 from clocking.api.view import (MacAddView, PersonAddView, PersonEditView,
-                               PersonListView, PersonClockingView)
+                               PersonListView, PersonClockingView, AboutView)
 
 api = Blueprint('api', __name__)
 api_manager = Manager()
@@ -12,3 +12,4 @@ api.add_url_rule('/add_mac', view_func=MacAddView.as_view('add_mac'))
 api.add_url_rule('/edit/<person_id>', view_func=PersonEditView.as_view('edit'))
 api.add_url_rule('/people', view_func=PersonListView.as_view('people'))
 api.add_url_rule('/', view_func=PersonClockingView.as_view('clocking'))
+api.add_url_rule('/about', view_func=AboutView.as_view('about'))
