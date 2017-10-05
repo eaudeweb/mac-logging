441d95ddfda88ac12edf40a8590cfe4c1a128fca
catalinjitea <catalinjitea@gmail.com>
2017-10-05 15:34:32 +0300

    Refactor clocking page and management commands

--- manage.py
+++ manage.py
@@ -21,27 +21,16 @@ def connect_to_db():
     return sqlite3.connect("/var/local/pontaj/files/mac_logging.db")
 
 
-@manager.command
-def clear_mac_addresses():
-    conn = connect_to_db()
-    c = conn.cursor()
-
-    c.execute("DELETE FROM mac_addresses")
-    conn.commit()
-
-    conn.close()
-
-
 def parse():
     infile = open("/var/local/pontaj/files/input.txt", "r")
-    mac_addresses = []
+    addresses = []
     for line in iter(infile):
         values = line.split(' "')
-        mac_addresses.append(values[1])
+        addresses.append(values[1])
 
     infile.close()
 
-    return mac_addresses
+    return addresses
 
 
 def get_time():
@@ -50,70 +39,47 @@ def get_time():
 
 
 @manager.command
-def check_insert_mac_addresses():
+def check_new_entries():
     conn = connect_to_db()
     c = conn.cursor()
 
-    c.execute("SELECT * FROM mac_addresses")
+    c.execute("SELECT * FROM address")
     existing_values = c.fetchall()
-    existing_mac_addresses = [pair[1] for pair in existing_values]
+    existing_addresses = [x[0] for x in existing_values]
 
-    mac_addresses = parse()
+    addresses = parse()
     last_modified = get_time()
-    c.execute("SELECT * FROM mac_addresses ORDER BY id DESC LIMIT 1")
+    new_entries = []
+    for address in addresses:
+        if address in existing_addresses:
+            c.execute("SELECT * FROM person JOIN address "
+                      "ON address.person_id = person.id "
+                      "AND address.mac=?", (address, ))
+            person_details = c.fetchone()
+            new_entries.append((address, person_details[1], person_details[2]))
+
+    c.execute("SELECT (id) FROM entry ORDER BY id DESC LIMIT 1")
     curr_id = c.fetchone()
     if curr_id is None:
         curr_id = 1
     else:
         curr_id = curr_id[0] + 1
+
     values = []
-    for mac_address in mac_addresses:
-        if mac_address not in existing_mac_addresses:
-            values.append((curr_id, mac_address, last_modified))
+
+    startdate_datetime = last_modified
+    startdate_string = datetime.strftime(startdate_datetime, '%Y-%m-%d %H:%M:%S')
+    for new_entry in new_entries:
+        c.execute("SELECT * FROM address a JOIN entry e ON a.mac = e.mac_id "
+                  "JOIN person p ON p.id = a.person_id "
+                  "WHERE p.last_name=? "
+                  "AND p.first_name=? "
+                  "AND DATE(e.startdate)=?", (new_entry[1], new_entry[2], startdate_datetime.date()))
+        if len(c.fetchall()) == 0:
+            values.append((curr_id, new_entry[0], startdate_string))
             curr_id += 1
 
-    conn.executemany("INSERT INTO mac_addresses VALUES (?, ?, ?)", values)
-    conn.commit()
-
-    conn.close()
-
-@manager.command
-def check_persons():
-    conn = connect_to_db()
-    c = conn.cursor()
-
-    c.execute("SELECT * FROM mac_addresses")
-    mac_addresses = c.fetchall()
-
-    values = []
-    c.execute("SELECT (id) FROM persons ORDER BY id DESC LIMIT 1")
-    curr_id = c.fetchone()
-    if curr_id is None:
-        curr_id = 1
-    else:
-        curr_id = curr_id[0] + 1
-    for mac_address in mac_addresses:
-        c.execute("SELECT * FROM persons_mac WHERE mac = ?", (mac_address[1], ))
-        persons_mac = c.fetchall()
-        for person_mac in persons_mac:
-            # Verify if this person is already registered for this day
-            my_date = mac_address[2]
-            my_date = datetime.strptime(my_date, '%Y-%m-%d %H:%M:%S')
-            c.execute("SELECT * FROM persons WHERE last_name=? "
-                      "AND first_name=? "
-                      "AND DATE(startdate)=?", (person_mac[2], person_mac[3],
-                                                my_date.date()
-                                                )
-                      )
-            if len(c.fetchall()) == 0:
-                startdate = mac_address[2]
-                enddate = startdate
-                enddate = datetime.strptime(enddate, '%Y-%m-%d %H:%M:%S')
-                enddate += timedelta(hours=8)
-                values.append((curr_id, person_mac[2], person_mac[3], startdate, enddate))
-                curr_id += 1
-
-    conn.executemany("INSERT INTO persons VALUES (?, ?, ?, ?, ?)", values)
+    conn.executemany("INSERT INTO entry VALUES (?, ?, ?)", values)
     conn.commit()
 
     conn.close()
--- tests/test_person.py
+++ tests/test_person.py
@@ -2,39 +2,50 @@ import json
 
 from flask import url_for
 
-from clocking.models import PersonMac
-from .factories import PersonMacFactory
+from tests.factories import PersonFactory
+from clocking.models import Person, Address
 
 
 def test_first_page(client):
     resp = client.get(url_for('api.clocking'))
     assert resp.status_code == 200
 
+
 def test_people(client):
     resp = client.get(url_for('api.people'))
     assert resp.status_code == 200
 
 
-def test_add_person_mac(client):
-    resp = client.post(url_for('api.add'), dict(
-        last_name='l', first_name='f',
-        mac1='ff:ff:ff:ff:ff:ff', mac2='', mac3=''))
+def test_add_person(client):
+    resp = client.post(url_for('api.add'),
+                       dict(last_name='l', first_name='f'))
     assert resp.status_code == 200
-    assert len(PersonMac.query.all()) == 1
-    assert PersonMac.query.first().mac == 'FF FF FF FF FF FF'
-    PersonMac.query.delete()
-    assert PersonMac.query.count() == 0
+    assert len(Person.query.all()) == 1
+    person = Person.query.first()
+    assert person.last_name == 'l'
+    assert person.first_name == 'f'
+    Person.query.delete()
+    assert Person.query.count() == 0
+
+
+def test_add_address(client):
+    person = PersonFactory()
+    resp = client.post(url_for('api.add_mac'),
+                       dict(mac='00:00:00:00:00:00', device='mobile',
+                            person=1))
+    assert resp.status_code == 200
+    assert len(Address.query.all()) == 1
+    address = Address.query.first()
+    assert address.mac == '00 00 00 00 00 00'
+    assert address.device == 'mobile'
 
 
 def test_edit_person_mac(client):
-    person_mac = PersonMacFactory()
-    resp = client.post(url_for('api.edit', person_id=1), dict(
-        last_name='test',
-        first_name='test',
-        mac='00:00:00:00:00:00'))
+    person = PersonFactory()
+    resp = client.post(url_for('api.edit', person_id=1),
+                       dict(last_name='l_test', first_name='f_test'))
     assert resp.status_code == 200
-    assert len(PersonMac.query.all()) == 1
-    person_mac = PersonMac.query.get(1)
-    assert person_mac.last_name == 'test'
-    assert person_mac.first_name == 'test'
-    assert person_mac.mac == '00 00 00 00 00 00'
+    assert len(Person.query.all()) == 1
+    person = Person.query.first()
+    assert person.last_name == 'l_test'
+    assert person.first_name == 'f_test'
