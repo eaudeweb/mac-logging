b5a3dc4b4d282fc2d9302e1ecc8dd95ce4156de7
catalinjitea <catalinjitea@gmail.com>
2017-10-09 14:55:06 +0300

    Fix clocking weekends

--- cronjobs-entrypoint.sh
+++ cronjobs-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Running cronjobs"
 
-echo "*/2 7-19 * * * /var/local/pontaj/crontabs/run_snmp.sh && python /var/local/pontaj/manage.py check_new_entries" > crontab.tmp
+echo "*/2 7-19 * * 1-5 /var/local/pontaj/crontabs/run_snmp.sh && python /var/local/pontaj/manage.py check_new_entries" > crontab.tmp
 
 crontab crontab.tmp
 rm crontab.tmp
