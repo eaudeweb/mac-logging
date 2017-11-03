16be251914f7fc89d298d8c27003c21c6c7cf924
catalinjitea <catalinjitea@gmail.com>
2017-11-03 13:52:25 +0200

    Add test for delete mac

--- tests/test_person.py
+++ tests/test_person.py
@@ -50,6 +50,23 @@ def test_edit_person_mac(client):
     assert person.last_name == 'l_test'
     assert person.first_name == 'f_test'
 
+
+def test_delete_person_mac(client):
+    person = PersonFactory()
+    resp = client.post(url_for('api.add_mac'),
+                       dict(mac='00:00:00:00:00:00', device='mobile',
+                            person=1))
+    assert resp.status_code == 200
+    assert len(Address.query.all()) == 1
+    address = Address.query.first()
+    assert address.deleted is False
+
+    resp = client.post(url_for('api.delete_mac', mac_address='00 00 00 00 00 00'))
+    assert resp.status_code == 200
+    address = Address.query.first()
+    assert address.deleted is True
+
+
 def test_about(client):
     resp = client.get(url_for('api.about'))
     assert resp.status_code == 200
