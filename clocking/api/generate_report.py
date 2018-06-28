45ab248c89f35c6c72c1fbab1c9a5fa021a389a6
catalinjitea <catalin.jitea@eaudeweb.ro>
2018-06-28 11:10:26 +0300

    Add departament column to Excel report

--- clocking/api/generate_report.py
+++ clocking/api/generate_report.py
@@ -13,8 +13,9 @@ def generate_report(entries):
         worksheet = workbook.add_worksheet(name=day)
         worksheet.write('A1', 'Name', bold)
         worksheet.write('B1', 'Device', bold)
-        worksheet.write('C1', 'Time In', bold)
-        worksheet.write('D1', 'Time Out', bold)
+        worksheet.write('C1', 'Departament', bold)
+        worksheet.write('D1', 'Time In', bold)
+        worksheet.write('E1', 'Time Out', bold)
         worksheets.append(worksheet)
     date_format = workbook.add_format({'num_format': 'hh:mm'})
     i = 0
@@ -24,17 +25,20 @@ def generate_report(entries):
         for entry_by_day in entry['entries_by_day']:
             name = entry_by_day.mac.person.first_name + ' ' + entry_by_day.mac.person.last_name
             device = entry_by_day.mac.device.value
+            dept = entry_by_day.mac.person.dept.name
             start_date = entry_by_day.startdate
             end_date = entry_by_day.startdate + timedelta(hours=8)
             worksheets[i].write_string(row, col, name)
             worksheets[i].write_string(row, col + 1, device)
-            worksheets[i].write_datetime(row, col + 2, start_date, date_format)
-            worksheets[i].write_datetime(row, col + 3, end_date, date_format)
+            worksheets[i].write_string(row, col + 2, dept)
+            worksheets[i].write_datetime(row, col + 3, start_date, date_format)
+            worksheets[i].write_datetime(row, col + 4, end_date, date_format)
             row += 1
         worksheets[i].set_column('A:A', 20)
         worksheets[i].set_column('B:B', 15)
-        worksheets[i].set_column('C:C', 15)
+        worksheets[i].set_column('C:C', 25)
         worksheets[i].set_column('D:D', 15)
+        worksheets[i].set_column('E:E', 15)
         i += 1
 
     workbook.close()
